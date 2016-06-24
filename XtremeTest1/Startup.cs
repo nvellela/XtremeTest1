43232651de9c8e6e3d2f32fc89906ff1ab7a5016
nare vellela <nare.vellela@nt.gov.au>
2016-06-24 18:39:37 +0930

    test

---
+++ XtremeTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XtremeTest1.Startup))]
+namespace XtremeTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
